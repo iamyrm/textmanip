1a0c3d618953bb13929a256ea51c5e021f35f0a6
Yagyaraj Majhi <majhiyagya123@gmail.com>
2022-10-23 14:28:18 +0545

    minor fix

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -27,9 +27,8 @@ function BasicExample() {
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
+          {/* <Button variant="primary" type="submit">Submit</Button> */}
+          <Button variant="primary" type="submit">Submit</Button>{' '}
         </Form>
 
       </div>
